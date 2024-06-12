1eda5edd1762ea062008f6ffd1a8e36000313305
maximiliano moraga villanueva <moraga.inf@gmail.com>
2024-06-12 18:04:34 -0400

    se agrega el modulo de registro de personas

--- funciones.py
+++ funciones.py
@@ -1,7 +1,14 @@
-def agregar(objeto, lista):
+from Persona import *
+
+def crear_persona(nombre, apellido, rut):
+    persona = Persona(nombre, apellido, rut)
+    return persona
+
+def agregar_al_registro(objeto, lista):
     lista.append(objeto)
-    
     
 def imprimir_lista(lista):
     for objeto in lista:
         print(objeto)
+        
+        
--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-# persona = Persona("max", "moraga", "111-1")
-# agregar(persona, lista_de_personas)
-# imprimir_lista(lista_de_personas)
-
 # Imports
-from Persona import *
 from funciones import *
 from menu import *
 
 lista_de_personas = []
 while True:
-    imprimir_menu()
-    input()
+    imprimir_menu(principal)
+    opcion_principal = input("Ingresar opcion: ")
+    # Opcion (1): Agregar un objeto a la lista
+    if opcion_principal == '1':
+        imprimir_mensaje("Se ha ingrsado a la popcion de nuevo resgistro de persona")
+        while True:
+            # Formulacion de registro
+            imprimir_mensaje("Nuevo registro")
+            # Ingres de dato: nombre, tipo: texto(str)
+            nombre = input("Ingrese el nombre: ")
+            # Ingres de dato: apellido, tipo: texto(str)
+            apellido = input("Ingrese el apellido: ")
+            # Ingres de dato: rut, tipo: texto(str)
+            rut = input("Ingrese el rut: ")
+            
+            # --
+            agregar_al_registro(crear_persona(nombre, apellido, rut), lista_de_personas)
+            imprimir_mensaje("Se ha agregado un objeto a la lista de personas.")
 
+            imprimir_menu(salida)
+            opcion_salida = input("Ingrese la opción")
+            if opcion_salida == '0':
+                imprimir_mensaje("saliendo de agregar registro")
+                break
+    
+    # Modificar un objeto de la lista
+    # Eliminar objeto de la lista
+    # Ver todos los objetos de la lista
+    # Buscar ???????????????????????
+    # Salir
+    if opcion_principal == '0':
+        imprimir_mensaje_de_salida()
+        break
 
 
 
